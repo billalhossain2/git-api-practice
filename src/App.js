8f3e21fb55dabbe7aff5e947ecee111150f7ee67
billal-webdev <billal.webdev@gmail.com>
2023-06-19 11:36:55 +0600

    React-api-practice

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import Form from "./components/Form";
-import Todos from "./components/Todos";
-import { useState } from "react";
 function App() {
-  let [todoTitle, setTodoTitle] = useState("");
-  let [todos, setTodos] = useState([]);
-  const [editableTodo, setEditableTodo] = useState(null);
   return (
     <div className="App">
-      <Form
-        todoTitle={todoTitle}
-        setTodoTitle={setTodoTitle}
-        todos={todos}
-        setTodos={setTodos}
-        editableTodo={editableTodo}
-        setEditableTodo={setEditableTodo}
-      />
-      <Todos
-        todos={todos}
-        setTodos={setTodos}
-        todoTitle={todoTitle}
-        setTodoTitle={setTodoTitle}
-        editableTodo={editableTodo}
-        setEditableTodo={setEditableTodo}
-      />
+      <h1>Hello</h1>
     </div>
   );
 }
